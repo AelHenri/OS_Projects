c8329ea1377a394f9d22a3bebf96102579b468d9
Henri <t.henri.al@gmail.com>
2017-02-02 21:02:50 -0600

    basic shell that does nothing

--- Project1/shell.c
+++ Project1/shell.c
@@ -1,42 +1,52 @@
 #include "shell.h"
-#define space" \t\r\n\a"
+#define space " \t\r\n\a"
+#define BUFFSIZE 64
 
 void type_prompt(){
 	printf("> ");
 }
 
-void read_command(char **command, char **parameters){
-	char* input
+void read_command(char *command, char *parameters[]){
+	int position = 0;
+	char* input;
+	char* buffer = malloc(sizeof(char *));
+	char** tokens = malloc(BUFFSIZE * sizeof(char *));
 	ssize_t bufsize=0;
-	getline(command, &bufsize, stdin);
-	input = strtok(command, space);
-	
+	getline(&buffer, &bufsize, stdin);
+	input = strtok(buffer, space);
+
 	while( input != NULL){
-		printf(" %s\n", input);
+		tokens[position] = input;
+		position++;
 		
 		input = strtok(NULL, space);
 	}
-		
 	
-	//command = cmd;
+	strcpy(command, tokens[0]);
+
+	for (int i = 0; i < position; ++i)
+	{
+		parameters[i] = malloc(sizeof(char *));
+		strcpy(parameters[i], tokens[i]);
+	}
 }
 
 int main(int argc, char const *argv[])
 {
-	char **command = malloc(sizeof(command));
-	char **parameters;
+	char *command = malloc(sizeof(char *));
+	char **parameters = malloc(BUFFSIZE * sizeof(char *));
 
 	int status;
 	while(TRUE){
 		type_prompt();
 		read_command(command,parameters);
-		printf("%s\n", *command);
-		/*if(fork()!=0){
+		printf("%s\n", command);
+		if(fork()!=0){
 			waitpid(-1,&status,0);
 		}	
 		else{
-			execve(*command, parameters, 0);
-		}*/
+			execve(command, parameters, 0);
+		}
 	}
 
 	free(command);
--- Project1/shell.h
+++ Project1/shell.h
@@ -3,9 +3,10 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <string.h>
 
 
 #define TRUE 1
 
 void type_prompt();
-void read_command(char **command, char **parameters);
+void read_command(char *command, char **parameters);
