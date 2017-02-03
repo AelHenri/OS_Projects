4bb41413718983367c8a2770ea8da263ecbf62c9
Henri <t.henri.al@gmail.com>
2017-02-02 19:57:48 -0600

    shell files

--- Project1/shell.c
+++ Project1/shell.c
@@ -5,19 +5,20 @@ void type_prompt(){
 
 }
 
-void read_command(command,parameters){
+void read_command(char *command, char *parameters){
 
 }
 
 int main(int argc, char const *argv[])
 {
 	while(TRUE){
+		char *command, parameters;
 		type_prompt();
 		read_command(command,parameters);
 		
 		if(fork()!=0){
 			waitpid(-1,&status,0);
-		}
+		}	
 		else{
 			execve(command, parameters, 0);
 		}
--- Project1/shell.h
+++ Project1/shell.h
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
 
 #define TRUE 1
 
 void type_prompt();
-void read_command(command,parameters);
+void read_command(char *command, char *parameters);
