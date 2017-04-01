4d49758dffff8e4376a7f021920629fe0817d941
Imane <Imane@dhcp41.merunorth103.iit.edu>
2017-04-01 12:27:43 -0500

    coucou

--- Project2/sys_coucou.h
+++ Project2/sys_coucou.h
@@ -8,5 +8,5 @@ int sys_tlookup(int topics_id[], int size);
 int sys_tcreate(int topic_id);
 int sys_tpublisher(int publisher_id, int topic_id);
 int sys_tsubscriber(int subscriber_id, int topic_id);
-int sys_tpublish(int publisher_id, int topic_id, char *msg);
+int sys_tpublish(int topic_id, int publisher_id, char *msg);
 int sys_tretrieve(int topic_id, int subscriber_id, char *mesg);
