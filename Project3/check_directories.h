d7a5c26c34f87a548eacf04e893b29276aa66860
irifki <rifkiimane@gmail.com>
2017-04-26 21:15:18 -0500

    Create check_directories.h

---
+++ Project3/check_directories.h
@@ -0,0 +1,20 @@
+#include <sys/cdefs.h>
+#include <fcntl.h>
+#include <lib.h>
+#include <stdio.h>
+#include <dirent.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <strings.h>
+#include <sys/types.h>
+#include "mfs/const.h"
+#include "mfs/inode.h"
+#include "mfs/type.h"
+#include "mfs/mfsdir.h"
+#include "mfs/super.h"
+#include "utilities.h"
+
+int view_directory_special_files(const char *path, int_list *imap);
+unsigned long check_special_directories(char path[]);
